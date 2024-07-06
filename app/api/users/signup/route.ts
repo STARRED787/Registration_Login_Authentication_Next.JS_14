835daed1696598c91a344f0ef0904adaccefc247
STARRED787 <malithamalshan7@gmail.com>
2024-07-06 15:28:29 +0530

    nextjs

--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -1,4 +1,12 @@
 import { connect } from "@/dbconfig/dbconfig";
 import User from "@/models/userModels";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 connect();
+export async function POST(request: NextRequest) {
+  try {
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }), { status: 500 };
+  }
+}
