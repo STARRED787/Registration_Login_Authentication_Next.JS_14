790038d6129161356e928234905649bfc45321a5
STARRED787 <malithamalshan7@gmail.com>
2024-07-06 21:36:41 +0530

    nextjs

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -2,3 +2,37 @@ import bcrypt from "bcryptjs";
 import { connect } from "@/dbconfig/dbconfig";
 import User from "@/models/userModels";
 import { NextRequest, NextResponse } from "next/server";
+import { error } from "console";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
+    }
+    // possword is coorect
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+
+    //create token data
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
