b73d8fd6b684d40011587646a7d9d63f08042b75
STARRED787 <malithamalshan7@gmail.com>
2024-07-06 16:26:44 +0530

    nextjs

--- dbconfig/dbconfig.ts
+++ dbconfig/dbconfig.ts
@@ -6,7 +6,7 @@ export async function connect() {
     const connection = mongoose.connection;
 
     connection.on("connected", () => {
-      console.log("MongoDB connected");
+      console.log("MongoDB connected successfuly");
     });
 
     connection.on("error", (err) => {
